4048874d5a018d7cd8f0059cd2c3b5b4e315b788
sushil chaulagain <sushilchlgn@gmail.com>
2025-02-21 13:28:28 +0545

    home

---
+++ resources/views/home/base.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{env("APP_NAME")}} - Taste Better</title>
+</head>
+<body>
+  .navbar.navbar-expand-large  
+</body>
+</html>
